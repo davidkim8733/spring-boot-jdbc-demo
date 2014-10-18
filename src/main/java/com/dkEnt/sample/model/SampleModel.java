3999a4bfcded650a2fcd2d705de552634f325620
Davidkim <davidkim8733@gmail.com>
2014-10-18 19:27:49 +0900

    dao commit
    dao commit

---
+++ src/main/java/com/dkEnt/sample/model/SampleModel.java
@@ -0,0 +1,22 @@
+package com.dkEnt.sample.model;
+
+public class SampleModel {
+
+	private String seq;
+	private String url;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+}
